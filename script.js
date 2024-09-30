3335a002e97e77ef172adc30b30171cd0030aa3f
Hana Veštšíková <vestsikovah@gmail.com>
2024-09-30 20:04:55 +0200

    verze 1

---
+++ script.js
@@ -0,0 +1,9 @@
+/* git add ., commit -m "text", push */
+
+const listek = 12
+const pocetPredstaveni = 15
+const navstevnost = 174
+const prijem = listek * navstevnost * pocetPredstaveni
+
+document.body.innerHTML = "prijem = " + prijem
+
